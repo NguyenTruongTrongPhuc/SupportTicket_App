3d030abc03332f0a47284459512595f307fea957
NguyenTruongTrongPhuc <155700379+NguyenTruongTrongPhuc@users.noreply.github.com>
2024-09-10 09:47:41 +0700

    Update ticket.py

--- addons/ticket_support/models/ticket.py
+++ addons/ticket_support/models/ticket.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api  # type: ignore
+import logging
 from datetime import datetime, timedelta
 
 class Ticket(models.Model):
@@ -6,9 +7,11 @@ class Ticket(models.Model):
     _description = 'Support Ticket'
     _rec_name = 'ticket_code'
 
-    is_customer = fields.Boolean(string="Is Customer", default = True)
-    # name = fields.Char(string = "Name", required = True)
-    
+    is_customer = fields.Boolean(string="Is Customer", default = False)
+    # partner issue the ticket
+    # both user and partner can issue the ticket (and both can be shown as res.partner)
+    partner_issue_ticket = fields.Many2one('res.partner', string="Customer")
+
     ################################
     #                              #
     #      Basic information       #                
@@ -16,112 +19,90 @@ class Ticket(models.Model):
     ################################
     ticket_code = fields.Char(string="Ticket Code",
                               readonly=True,
-                              #computed="_compute_ticket_code",
-                              store=True,
+                              compute="_compute_ticket_code",
+                              store = True,
                               help="Ticket Code"
                               )
     name = fields.Char(string="Ticker Title",
-                               required=True,
-                               help="Ticket Title"
-                               )
-    custom_create_date = fields.Datetime(string="Create Day")
-    nearest_response_date = fields.Datetime(string="Response Day")
+                       required=True,
+                       help="Ticket Title"
+                       )
+    custom_create_date = fields.Datetime(string="Create Day", default=fields.Datetime.now)
+
     stage = fields.Many2one('support.ticket.stage',
                             string="Ticket Stage",
-                            readonly = True,
                             group_expand="_read_group_stage"  # for getting all the stage
                             )
+    # left side is the value, right side is the string that is shown
+    # we can utilizing the left size to store the color code
+    status = fields.Selection([
+        ("10", "New"),
+        ("3", "In Week"),
+        ("1", "In Month"),
+        ],
+        string="Status")
+
     content = fields.Html(string="Ticket description", help="Description")
 
-    status = fields.Many2one('support.ticket.status', string="Status", readonly = True)
+    ticket_additional = fields.One2many('support.ticket', 'parent_ticket_id', string="Ticket Response")
 
-    ticket_response = fields.One2many('support.ticket', 'parent_ticket_id', string="Ticket Response")
-    note = fields.Text(string="Note")
-    state = fields.Selection([
-        ('new', 'New'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done')
-    ], default='new', string="State")
+    note = fields.Html(string="Note")
 
-    # internal user
-    user = fields.Many2many('res.users', string="User")
-    user_image = fields.Binary(related='user.image_1920', string='User Image')
+    # inhouse user to handle the ticket
+    user_handling = fields.Many2many('res.users', string="User")
+    user_handling_image = fields.Binary(related='user_handling.image_1920', string='User Image')
 
     ####################
     #                  #
     #     customer     #
     #                  #
     ####################
-    customer_id = fields.Many2one('res.partner', string="Customer")
-    email = fields.Char(string="Email", related="customer_id.email")
-    phone = fields.Char(string="Phone", related="customer_id.phone")
-    website = fields.Char(string="Website", related="customer_id.website")
+    email = fields.Char(string="Email", related="partner_issue_ticket.email")
+    phone = fields.Char(string="Phone", related="partner_issue_ticket.phone")
+    website = fields.Char(string="Website", related="partner_issue_ticket.website")
 
     # Added Many2one field to establish the inverse relation
     parent_ticket_id = fields.Many2one('support.ticket', string="Parent Ticket")
-    ticket_type = fields.Selection([
-        ('type1', 'Type 1'),
-        ('type2', 'Type 2'),
-        ('type3', 'Type 3'),
-    ], string="Ticket Type")
+
+    ticket_type = fields.Many2one('support.ticket.type', string="Ticket Type")
+
+    
+    # @api.model
+    # def create(self,vals):
+    #     res = super(Ticket, self).create(vals)
+    #     res.update_status()
+    #     res.compute_color()
+    
     
     @api.model
-    def _read_group_stage(self, group, domain,order):
-        return self.env['support.ticket.status'].search([])
-
-
-    def action_send_email(self):
-        mail_template = self.env.ref('real-estate-ads.offer_mail_template')
-        mail_template.send_mail(self.id, force_send=True)
-
-    # @api.depends('id')
-    # def _compute_ticket_code(self):
-    #     pass
+    def _read_group_stage(self, group, domain, order):
+        return self.env['support.ticket.stage'].search([])
     
-    @api.onchange('stage')
-    def _onchange_stage(self):
-        if self.stage:
-            self.status = self.stage.status
-
-    # def action_new(self):
-    #     self.state = 'new'
-        
-    # def action_in_progress(self):
-    #     self.state = 'in_progress'
-        
-    # def action_done(self):
-    #     self.state = 'done'
-        
-    # status_class = fields.Char(compute='_compute_status_class', string="Status Class")
-    
-    color = fields.Integer(string="Color", compute = "update_ticket_status_color")
+    color = fields.Integer(string="Color", compute="compute_color")
 
     @api.model
-    def update_ticket_status_color(self):
-        today = fields.Datetime.now()
-        tickets = self.env['support.ticket'].search([])
-        for ticket in tickets:
+    def update_status(self):
+        datetime_now = datetime.now()
+        for ticket in self.search([]):
             if ticket.custom_create_date:
-                valid_days = (today - ticket.custom_create_date).days
-                if valid_days < 1:
-                    ticket.color = 10 # Green
-                elif 1 <= valid_days <= 7:
-                    ticket.color = 3 # Yellow
+                create_date = fields.Datetime.from_string(ticket.custom_create_date)
+                if (datetime_now - create_date).days <= 1:
+                    ticket.status = "10"
+                elif(datetime_now - create_date).days <= 7:
+                    ticket.status = "3"
                 else:
-                    ticket.color = 1 # Red
+                    ticket.status = "1"
+                ticket.compute_color()
+
+    @api.depends("status")
+    def compute_color(self):
+        for rec in self:
+            if rec.status:
+                rec.color = int(rec.status)
             else:
-                ticket.color = 0
-                
-    # def write(self, vals):
-    #     if 'stage_id' in vals:
-    #         stage = self.env['support.ticket.stage'].browse(vals['stage_id'])
-    #         if stage.status_id:
-    #             vals['status'] = stage.status_id.id
-    #     return super(Ticket, self).write(vals)
-    
+                rec.color = 0
 
-                
-    boolean_toggle = fields.Boolean(string="User Mode", default=False)
 
-    
-    
+    def _compute_ticket_code(self):
+        for ticket in self:
+            ticket.ticket_code = "TK" + str(ticket.id).zfill(10)           
